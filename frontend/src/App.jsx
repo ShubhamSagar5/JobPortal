3dbe3f89ec4345020a470fbfb12e65397e60670c
shubham <shubhamsagar628@gmail.com>
2025-01-24 20:10:07 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { deepOrange, deepPurple } from '@mui/material/colors';
+import Avatar from '@mui/material/Avatar';
+
+
+const App = () => {
+  return (
+    <div>App</div>
+          <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+
+  )
+}
+
+export default App
