26e9f3be4d7b063107750fd51917d23822be3956
shubham <shubhamsagar628@gmail.com>
2025-01-27 20:45:59 +0530

    Redux Store Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {ToastContainer} from 'react-toastify'
+import {Provider} from 'react-redux'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <ToastContainer/>
+  <Provider store={store}>
+  <App />
+  <ToastContainer/>
+  </Provider>
+    
   </StrictMode>,
 )
